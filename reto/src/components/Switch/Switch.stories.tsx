ba6e68b85e617a88d8db1f7ed191121dc987d99c
Sergei Naumov <iquink@gmail.com>
2025-05-21 10:21:52 +0300

    feat: add switch stories

---
+++ reto/src/components/Switch/Switch.stories.tsx
@@ -0,0 +1,70 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Switch from "./Switch";
+import { ThemeProvider } from "../../context/ThemeContext";
+import React from "react";
+
+// Meta information for the component
+const meta = {
+  title: "Components/Switch",
+  component: Switch,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      control: "text",
+      description: "Visible label for the switch",
+    },
+    isDisabled: {
+      control: "boolean",
+      description: "Whether the switch is disabled",
+    },
+    isThemeSwitch: {
+      control: "boolean",
+      description: "Whether the switch is used for theme switching",
+    },
+    onChange: { action: "toggled" },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} satisfies Meta<typeof Switch>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// Default switch story
+export const Default: Story = {
+  args: {
+    label: "Default Switch",
+  },
+};
+
+// Disabled switch story
+export const Disabled: Story = {
+  args: {
+    label: "Disabled Switch",
+    isDisabled: true,
+  },
+};
+
+// Theme switch story
+export const ThemeSwitch: Story = {
+  args: {
+    label: "Theme Switch",
+    isThemeSwitch: true,
+  },
+};
+
+// Custom class switch story
+export const CustomClass: Story = {
+  args: {
+    label: "Custom Class Switch",
+    className: "custom-class",
+  },
+};
