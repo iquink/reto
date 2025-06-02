8c55d8da26acf0eb9d2a07f00b4e9bf169366204
Sergei Naumov <iquink@gmail.com>
2025-06-02 14:29:39 +0300

    chore: some testing stuff

--- reto/src/pages/Home.tsx
+++ reto/src/pages/Home.tsx
@@ -3,10 +3,17 @@ import { useStore } from "@store";
 import { observer } from "mobx-react-lite";
 import { Select } from "@components/index";
 import { useTranslation } from "react-i18next";
+import i18next from "i18next";
 
 const Home: React.FC = observer(() => {
   const { authStore, settingsStore } = useStore();
   const { t } = useTranslation();
+
+  const onLanguageChange = (value: string) => {
+    console.log("Selected language:", value);
+  };
+
+  console.log("Current i18next language:", i18next.language);
 
   return (
     <div>
@@ -18,12 +25,14 @@ const Home: React.FC = observer(() => {
           : "No user logged in"}
       </p>
       <Select
-        label="Select an option"
         name="options"
+        aria-label="Select an option"
+        onSelectionChange={(e) => onLanguageChange(String(e))}
+        defaultSelectedKey={"1"}
         options={[
-          { value: "option1", label: "Option 1" },
-          { value: "option2", label: "Option 2" },
-          { value: "option3", label: "Option 3" },
+          { value: "1", label: "Option 1" },
+          { value: "2", label: "Option 2" },
+          { value: "3", label: "Option 3" },
         ]}
         onChange={(value) => console.log("Selected:", value)}
       />
