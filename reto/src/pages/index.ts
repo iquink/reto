5035d3fb644f061e12cf8b7527f4bbb0f57b6c83
Sergei Naumov <iquink@gmail.com>
2025-06-13 10:53:18 +0300

    refactor(frontend): move nested pages

--- reto/src/pages/index.ts
+++ reto/src/pages/index.ts
@@ -2,7 +2,7 @@ import Home from "./Home";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
 import Profile from "./Profile";
-import AddIssue from "./AddIssue/AddIssue";
+import AddIssue from "./Issues/AddIssue/AddIssue";
 import Issues from "./Issues/Issues";
 import Issue from "./Issue/Issue";
 import Admin from "./Admin/Admin";
